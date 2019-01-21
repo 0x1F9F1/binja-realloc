7aacf3f73ceb44fc1519a3d6bad64bde187bc4eb
Brick <6098371+0x1F9F1@users.noreply.github.com>
2019-01-21 15:32:47 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Binja Realloc
+Author: **Brick**
+
+A Binary Ninja plugin to fix realloc usage
---
+++ src/main.cpp
@@ -0,0 +1,112 @@
+/*
+    Copyright 2018 Brick
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+    and associated documentation files (the "Software"), to deal in the Software without restriction,
+    including without limitation the rights to use, copy, modify, merge, publish, distribute,
+    sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or
+    substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <malloc.h>
+#include <utility>
+
+#include <string>
+
+#define RVA2VA(type, base, rva) (type)((ULONG_PTR)base + rva)
+
+// https://github.com/stevemk14ebr/PolyHook_2_0/blob/master/sources/IatHook.cpp
+IMAGE_THUNK_DATA* FindIatThunk(void* moduleBase, const char* funcName)
+{
+    if (moduleBase == nullptr)
+        return nullptr;
+
+    IMAGE_DOS_HEADER* pDos = (IMAGE_DOS_HEADER*)moduleBase;
+    IMAGE_NT_HEADERS* pNT = RVA2VA(IMAGE_NT_HEADERS*, moduleBase, pDos->e_lfanew);
+    IMAGE_DATA_DIRECTORY* pDataDir = (IMAGE_DATA_DIRECTORY*)pNT->OptionalHeader.DataDirectory;
+
+    if (pDataDir[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress == NULL) {
+        return nullptr;
+    }
+
+    IMAGE_IMPORT_DESCRIPTOR* pImports = (IMAGE_IMPORT_DESCRIPTOR*)RVA2VA(uintptr_t, moduleBase, pDataDir[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+
+    // import entry with null fields marks end
+    for (uint_fast16_t i = 0; pImports[i].Name != NULL; i++) {
+        // Original holds the API Names
+        PIMAGE_THUNK_DATA pOriginalThunk = (PIMAGE_THUNK_DATA)
+            RVA2VA(uintptr_t, moduleBase, pImports[i].OriginalFirstThunk);
+
+        // FirstThunk is overwritten by loader with API addresses, we change this
+        PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)
+            RVA2VA(uintptr_t, moduleBase, pImports[i].FirstThunk);
+
+        if (!pOriginalThunk) {
+            return nullptr;
+        }
+
+        // Table is null terminated, increment both tables
+        for (; pOriginalThunk->u1.Ordinal != NULL; pOriginalThunk++, pThunk++) {
+            if (IMAGE_SNAP_BY_ORDINAL(pOriginalThunk->u1.Ordinal)) {
+                continue;
+            }
+
+            PIMAGE_IMPORT_BY_NAME pImport = (PIMAGE_IMPORT_BY_NAME)
+                RVA2VA(uintptr_t, moduleBase, pOriginalThunk->u1.AddressOfData);
+
+            if (strcmp(pImport->Name, funcName))
+                continue;
+
+            return pThunk;
+        }
+    }
+
+    return nullptr;
+}
+
+using realloc_t = void* (*)(void*, size_t);
+
+realloc_t realloc_orig = nullptr;
+
+void* realloc_hook(void* ptr, size_t size)
+{
+    if (ptr) {
+        size_t old_size = _msize(ptr);
+
+        if (size > (old_size >> 1))
+        {
+            if (size < old_size)
+                return ptr;
+
+            size_t new_size = old_size + (old_size >> 1);
+
+            if (size < new_size)
+                size = new_size;
+        }
+    }
+
+    return realloc_orig(ptr, size);
+}
+
+extern "C" __declspec(dllexport) bool CorePluginInit()
+{
+    realloc_t* realloc_iat = (realloc_t*)FindIatThunk((void*)GetModuleHandleA("binaryninjacore.dll"), "realloc");
+
+    DWORD old_protect;
+    VirtualProtect(realloc_iat, sizeof(*realloc_iat), PAGE_READWRITE, &old_protect);
+    realloc_orig = std::exchange(*realloc_iat, &realloc_hook);
+    VirtualProtect(realloc_iat, sizeof(*realloc_iat), old_protect, &old_protect);
+
+    return true;
+}
